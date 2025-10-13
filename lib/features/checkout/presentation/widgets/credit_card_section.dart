274fce9cba64524530314394e53ff31a9c16fc6c
hosamafiky <hosamafiky@gmail.com>
2025-10-13 23:51:20 +0300

    refactor: Remove unused equatable dependency and related model from credit card section

--- lib/features/checkout/presentation/widgets/credit_card_section.dart
+++ lib/features/checkout/presentation/widgets/credit_card_section.dart
@@ -1,23 +1,5 @@
 part of '../../imports.dart';
 
-// ignore: must_be_immutable
-class MyCreditCardModel extends CreditCardModel with EquatableMixin {
-  MyCreditCardModel({String cardNumber = '', String expiryDate = '', String cardHolderName = '', String cvvCode = '', bool isCvvFocused = false})
-    : super(cardNumber, expiryDate, cardHolderName, cvvCode, isCvvFocused);
-
-  factory MyCreditCardModel.empty() => MyCreditCardModel();
-  factory MyCreditCardModel.fromCreditCardModel(CreditCardModel model) => MyCreditCardModel(
-    cardNumber: model.cardNumber,
-    expiryDate: model.expiryDate,
-    cardHolderName: model.cardHolderName,
-    cvvCode: model.cvvCode,
-    isCvvFocused: model.isCvvFocused,
-  );
-
-  @override
-  List<Object?> get props => [cardNumber, expiryDate, cardHolderName, cvvCode, isCvvFocused];
-}
-
 class CreditCardSection extends StatefulWidget {
   const CreditCardSection({super.key, required this.formKey});
 
@@ -28,12 +10,8 @@ class CreditCardSection extends StatefulWidget {
 }
 
 class _CreditCardSectionState extends State<CreditCardSection> {
-  MyCreditCardModel cardModel = MyCreditCardModel();
-  void onCreditCardModelChange(CreditCardModel model) {
-    setState(() {
-      cardModel = MyCreditCardModel.fromCreditCardModel(model);
-    });
-  }
+  CreditCardModel cardModel = CreditCardModel('', '', '', '', false);
+  void onCreditCardModelChange(CreditCardModel model) => setState(() => cardModel = model);
 
   @override
   Widget build(BuildContext context) {
